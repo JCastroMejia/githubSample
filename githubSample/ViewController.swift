aabdb70e46da5b357498a54b8905ab5c266e7189
JASON CASTRO-MEJIA <rl239917@rlas-macair-35.rlas-116.org>
2018-09-24 10:50:05 -0500

    Initial Commit

---
+++ githubSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  githubSample
+//
+//  Created by JASON CASTRO-MEJIA on 9/24/18.
+//  Copyright © 2018 JASON CASTRO-MEJIA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
